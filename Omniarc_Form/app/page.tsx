1edb84dfc9434b832158debcfe311338db1ae8ee
BelvynAi <tbpvision101@gmail.com>
2025-11-03 23:00:02 +0100

    Add files via upload

---
+++ Omniarc_Form/app/page.tsx
@@ -0,0 +1,9 @@
+import { OmniarcCustomizationForm } from "@/components/omniarc-customization-form"
+
+export default function Page() {
+  return (
+    <div className="min-h-screen bg-background">
+      <OmniarcCustomizationForm />
+    </div>
+  )
+}
