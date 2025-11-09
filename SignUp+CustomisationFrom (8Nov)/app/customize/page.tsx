7961e1b471c927b6177394b5688451a3ce4e01f2
BelvynAi <tbpvision101@gmail.com>
2025-11-09 01:17:20 +0100

    Delete SignUp+CustomisationFrom (8Nov) directory

--- SignUp+CustomisationFrom (8Nov)/app/customize/page.tsx
+++
@@ -1,9 +0,0 @@
-import { OmniarcCustomizationForm } from "@/components/omniarc-customization-form"
-
-export default function CustomizePage() {
-  return (
-    <div className="min-h-screen bg-background">
-      <OmniarcCustomizationForm />
-    </div>
-  )
-}
